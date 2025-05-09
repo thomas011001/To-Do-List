f3cbd49d579c2fba7c1f81b9398bb3fae1bdf8c0
thomas <thomasyacoub321@gmail.com>
2025-05-10 02:56:29 +0300

    edited the config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-	mode: "production",
-	entry: "./src/js/main.js",
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	devtool: "eval-source-map",
-	devServer: {
-		watchFiles: ["./src/template.html"],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-		new BundleAnalyzerPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
+  mode: "production",
+  entry: "./src/js/main.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
