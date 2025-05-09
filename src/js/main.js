7949f8330891ad96bc3a5d1427b0f8b33b8270d4
thomas <thomasyacoub321@gmail.com>
2025-05-10 02:56:44 +0300

    added the elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
-import "../css/normalizer.css"
-import "../css/styles.css"
+import "../css/styles.css";
+import "../css/nav.css";
+import "../css/main.css";
+import storage from "./modules/storage";
+import projectCoreFactory from "./modules/project";
+import TaskCoreFactory from "./modules/todo";
+
+const def = projectCoreFactory(storage.getProject("default"));
+console.log(def);
+
+def.show();
+
+// document.getElementById("defaultProject").addEventListener("click");
