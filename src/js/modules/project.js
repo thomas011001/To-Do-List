054e69f678921cc2ff256d377c30f33ba15adc71
thomas <thomasyacoub321@gmail.com>
2025-05-10 02:55:35 +0300

    edited the modules

--- src/js/modules/project.js
+++ src/js/modules/project.js
@@ -1,6 +1,50 @@
-function projectCoreFactory(title, description) {
-    return {
-        title,
-        description,
-    }
+import TaskCoreFactory from "./todo";
+import storage from "./storage";
+
+function projectCoreFactory(project) {
+  const title = project.title;
+
+  const description = project.description;
+
+  const tasks = project.tasks || [];
+
+  const show = () => {
+    const container = document.getElementById("tasks");
+    container.innerHTML = `
+    <h1><div class="container">Default</div></h1>
+    <p><div class="container"> This is the default project for you.</div></p>
+    <div id="projectTasks" class="project-tasks">
+      <div class="container">
+
+      </div>
+    </div>
+    <div class="container">
+      <input type="text" id="taskName"/>
+      <button id="addTask">
+        + New Task
+      </button>
+    </div>
+    `;
+
+    document.getElementById("addTask").addEventListener("click", () => {
+      const taskTitle = document.getElementById("taskName").value;
+      document.getElementById("taskName").value = "";
+      project = title;
+
+      const task = { title: taskTitle, project };
+
+      storage.addTask(task);
+    });
+
+    tasks.forEach((task) => TaskCoreFactory(task).show());
+  };
+
+  return {
+    title,
+    description,
+    tasks,
+    show,
+  };
 }
+
+export default projectCoreFactory;
--- src/js/modules/todo.js
+++ src/js/modules/todo.js
@@ -1,14 +1,33 @@
-function TaskCoreFactory(title, description, priority, project) {
-    let finished = false;
-    const getFinished = () => finished
-    const setFinished = V => finished = V
-    return {
-        title,
-        description,
-        priority,
-        project,
-        setFinished,
-        getFinished
-    }
+import storage from "./storage";
+
+function TaskCoreFactory(task) {
+  let finished = task.finished || false;
+
+  const title = task.title;
+
+  const setFinished = (v) => (finished = v);
+
+  const show = () => {
+    const container = document.querySelector("#projectTasks .container");
+    const task = document.createElement("div");
+    task.className = "task";
+    task.innerHTML = `
+			<input type="checkbox" ${finished ? "checked" : ""}>${title}</div>
+		`;
+    container.appendChild(task);
+
+    // task.querySelector("input").addEventListener("click", {
+    // 	finished = !finished
+
+    // 	storage.editTask("default", )
+    // });
+  };
+
+  return {
+    title,
+    setFinished,
+    show,
+  };
 }
 
+export default TaskCoreFactory;
